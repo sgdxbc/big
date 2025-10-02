ab32e539b86f2b91530e9fc88563e254ff3c4fe9
sgdxbc <sung@comp.nus.edu.sg>
2025-10-02 17:01:50 +0800

    chore: update uv spec

--- control/terraform/main.tf
+++ control/terraform/main.tf
@@ -33,8 +33,9 @@ data "aws_ami" "ubuntu" {
   most_recent = true
 
   filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
+    name = "name"
+    # values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-20251001"]
   }
 
   owners = ["099720109477"] # Canonical
